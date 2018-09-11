b5ff9def44d5835f3dbf493abc80c0e048dce624
Tom Lau <tomoslau@gmail.com>
2018-09-11 23:01:06 +0800

    Content and styling changes

---
+++ _site/js/fade.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('.tofadein').addClass("fadein");
+    $('.tofadein-second').addClass("fadein move delay");
+
+    /* Check the location of each desired element */
+    $('.scrollreveal').each( function(i){
+        
+        var top_of_object = $(this).position().top;
+        var middle_of_object = $(this).position().top + $(this).outerHeight() / 2;
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();            
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > top_of_object ){
+            
+            $(this).addClass( "fadein move delay" );
+                
+        }
+        
+    }); 
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.scrollreveal').each( function(i){
+            
+            var top_of_object = $(this).position().top;
+            var middle_of_object = $(this).position().top + $(this).outerHeight() / 2;
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();            
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > middle_of_object ){
+                
+                $(this).addClass( "fadein move" );
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
