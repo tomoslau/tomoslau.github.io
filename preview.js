579401bd960a5e0f932eb1f9c96198aeb42efb3e
tomoslau <tomoslau@gmail.com>
2024-03-31 19:16:10 -0700

    d fix

--- preview.js
+++ preview.js
@@ -58,16 +58,22 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
                                                 : previewReset()
     }
 } else {
-    document.onmouseover = function (e) {
-        e.target == cdc ? previewCdc()
-            : e.target == tectonic ? previewTectonic()
-                : e.target == wristcheck ? previewWristcheck()
-                    : e.target == dhgroupordering ? previewDhgroupordering()
-                        : e.target == dhpreorder ? previewDhpreorder()
-                            : e.target == gogoxdelivery ? previewGogoxdelivery()
-                                : e.target == ggb ? previewGgb()
-                                    : e.target == hypebeast ? previewHypebeast()
-                                        : e.target == hbx ? previewHbx()
-                                            : previewReset()
-    }
+    cdc.addEventListener("mouseenter", previewCdc);
+    cdc.addEventListener("mouseleave", previewReset);
+    tectonic.addEventListener("mouseenter", previewTectonic);
+    tectonic.addEventListener("mouseleave", previewReset);
+    wristcheck.addEventListener("mouseenter", previewWristcheck);
+    wristcheck.addEventListener("mouseleave", previewReset);
+    dhgroupordering.addEventListener("mouseenter", previewDhgroupordering);
+    dhgroupordering.addEventListener("mouseleave", previewReset);
+    dhpreorder.addEventListener("mouseenter", previewDhpreorder);
+    dhpreorder.addEventListener("mouseleave", previewReset);
+    gogoxdelivery.addEventListener("mouseenter", previewGogoxdelivery);
+    gogoxdelivery.addEventListener("mouseleave", previewReset);
+    ggb.addEventListener("mouseenter", previewGgb);
+    ggb.addEventListener("mouseleave", previewReset);
+    hypebeast.addEventListener("mouseenter", previewHypebeast);
+    hypebeast.addEventListener("mouseleave", previewReset);
+    hbx.addEventListener("mouseenter", previewHbx);
+    hbx.addEventListener("mouseleave", previewReset);
 }
